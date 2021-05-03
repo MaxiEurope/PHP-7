c46664ab06592ef9d9bc2a2fe827fbe9871b9515
MaxiEurope <melmaxik@gmail.com>
2021-05-03 21:57:34 +0200

    :eyes:

--- tests/may1/test.php
+++ tests/may1/test.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html>
-<body>
+<style>
+h1 { text-align: center; }
+</style>
+
+<body style="background-color:#36393f;">
+<h1>😂</h1>
 
 <?php
-print("yo")
+for ($i = 0;$i < 53;$i++) {
+  $color = sprintf("#%06x",rand(0,16777215));
+  print ($i % 4 === 0 ? "<span style=\"color:$color\"> ${i} TRUE</span><br>" : "<span style=\"color:#ffffff\">$i FALSE</span><br>");
+}
 ?>
 
 </body>
